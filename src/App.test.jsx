f2a072a224d4c876de1b0e85733e04e79338c3d3
Kamie Short <kamie.short@gmail.com>
2022-05-23 12:19:08 -0700

    test passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,14 +4,13 @@ import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 describe('App', () => {
-  it('Should render list of books', async () => {
+  it('Should display messages', async () => {
     render(
       <MemoryRouter>
         <App />
       </MemoryRouter>
     );
 
-    const heading = screen.getByText('Simple Chat');
-    expect(heading).toBeInTheDocument();
+    screen.getByRole('link', { name: /💬 simple chat/i });
   });
 });
