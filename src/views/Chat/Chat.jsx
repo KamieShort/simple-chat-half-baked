ec52567aa847196d7d2b98d96b9ce3818cf735d8
Kamie Short <kamie.short@gmail.com>
2022-05-23 11:57:16 -0700

    messages displaying corectly

--- src/views/Chat/Chat.jsx
+++ src/views/Chat/Chat.jsx
@@ -26,13 +26,15 @@ export default function Chat() {
   useEffect(() => {
     getMessages()
       .then(setMessages)
+      .then(subscribe)
       // TODO: Subscribe to message changes, using handleMessageReceived
       // as the callback function
+
       // Don't forget to return a cleanup method (`unsubscribe`)
       // from this useEffect!
       .catch(console.error)
       .finally(() => unsubscribe);
-  }, []);
+  }, [handleMessageReceived]);
 
   return (
     <main className={styles.container}>
